8003ddcfc4f4e0e67e9fc93d800deb6c1aa39056
Nozomi Ito <nosonosonoso@gmail.com>
2019-11-18 17:30:54 +0900

    Update test app and use ios13.2

--- test/testAppium.js
+++ test/testAppium.js
@@ -45,7 +45,7 @@ function iOS13SimulatorForUdidBaseCapabilities (udid) {
 function iOS13SimulatorBaseCapabilities () {
   return {
     platformName: 'iOS',
-    platformVersion: '13.1',
+    platformVersion: '13.2',
     deviceName: 'iPhone 8',
     automationName: 'XCUITest',
     showXcodeLog: true,
@@ -400,10 +400,10 @@ describe("Appium", function () {
       ['bundleId', 'com.apple.Preferences', false]
     ])
     .it("should work with headless udid iOS simulator13: %s=%s", async (targetKey, targetValue, additionalCheck) => {
-      let devices = (await nodeSimctl.getDevices())["13.1"];
+      let devices = (await nodeSimctl.getDevices())["13.2"];
       devices = devices.filter((device) => device.name.indexOf("iPhone 8") != -1);
       if (devices.length == 0) {
-        throw new Error("cannot find the simulator for iOS 13.1 and iPhone 8. Please prepare it.");
+        throw new Error("cannot find the simulator for iOS 13.2 and iPhone 8. Please prepare it.");
       }
       let udid = devices[0].udid;
       let caps = iOS13SimulatorForUdidBaseCapabilities(udid);
